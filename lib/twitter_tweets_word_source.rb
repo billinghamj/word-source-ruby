9156e7cf8491fe278d0cffc01e6e0510f672f442
James Billingham <james@billingham.net>
2014-10-27 20:51:32 +0000

    Added word source which can work based on Twitter tweets

---
+++ lib/twitter_tweets_word_source.rb
@@ -0,0 +1,19 @@
+require './stream_word_source'
+require 'stringio'
+
+class TwitterTweetsWordSource < StreamWordSource
+	def initialize(tweets, options = {})
+		io = StringIO.new
+
+		tweets.each do |tweet|
+			text = tweet.text.gsub /\s+/, ','
+			io.write ','
+			io.write text
+		end
+
+		io.rewind
+		io.seek 1 # skip past leading comma
+
+		super io, options
+	end
+end
